feee3bc3bd8cf799f344c155bd46571ea205ad30
Antraxmin <antraxmin@naver.com>
2025-05-02 20:27:50 +0900

    feat: util func that converts memory limits in string form to actual bytes

--- pkg/cgroups/utils.go
+++ pkg/cgroups/utils.go
@@ -1,6 +1,11 @@
 package cgroups
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func readFile(path string) (string, error) {
 	data, err := os.ReadFile(path)
@@ -12,4 +17,27 @@ func readFile(path string) (string, error) {
 
 func writeFile(path, data string) error {
 	return os.WriteFile(path, []byte(data), 0644)
+}
+
+func parseMemoryLimit(limit string) (int64, error) {
+	limit = strings.ToLower(limit)
+
+	var multiplier int64 = 1
+	if strings.HasSuffix(limit, "k") {
+		multiplier = 1024
+		limit = limit[:len(limit)-1]
+	} else if strings.HasSuffix(limit, "m") {
+		multiplier = 1024 * 1024
+		limit = limit[:len(limit)-1]
+	} else if strings.HasSuffix(limit, "g") {
+		multiplier = 1024 * 1024 * 1024
+		limit = limit[:len(limit)-1]
+	}
+
+	value, err := strconv.ParseInt(limit, 10, 64)
+	if err != nil {
+		return 0, fmt.Errorf("Invalid memory limit format: %s", limit)
+	}
+
+	return value * multiplier, nil
 }
