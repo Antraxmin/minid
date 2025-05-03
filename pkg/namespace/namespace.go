d22af8794d6cc932356440aafce0a87eef814e50
Antraxmin <antraxmin@naver.com>
2025-05-03 12:53:22 +0900

    setup veth pairs for container network connections

--- pkg/namespace/namespace.go
+++ pkg/namespace/namespace.go
@@ -17,3 +17,31 @@ func DeleteNetworkNamespace(name string) error {
 	}
 	return nil
 }
+
+func SetupVethPair(containerID, bridgeName string) error {
+	// create veth name
+	vethName := fmt.Sprintf("veth-%s", containerID[:8])
+	peerName := fmt.Sprintf("eth0-%s", containerID[:8])
+
+	// create veth pair
+	if err := executeCommand("ip", "link", "add", vethName, "type", "veth", "peer", "name", peerName); err != nil {
+		return fmt.Errorf("failed to create veth pair: %v", err)
+	}
+
+	// connect to veth bridge
+	if err := executeCommand("ip", "link", "set", vethName, "master", bridgeName); err != nil {
+		return fmt.Errorf("failed to connect veth to bridge: %v", err)
+	}
+
+	// activate veth
+	if err := executeCommand("ip", "link", "set", vethName, "up"); err != nil {
+		return fmt.Errorf("failed to activate veth: %v", err)
+	}
+
+	// Move peer to container namespace
+	if err := executeCommand("ip", "link", "set", peerName, "netns", containerID); err != nil {
+		return fmt.Errorf("failed to move peer to container namespace: %v", err)
+	}
+
+	return nil
+}
